e1b6080319b22c0248706eca6341be7f1480ec04
YoungBaeeee <jjolba@naver.com>
2017-04-16 03:53:50 +0000

    게시물 화면구성 및 http staus 404 처리

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # blog/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Post
 
 def post_list(request):
@@ -16,3 +17,17 @@ def post_list(request):
                       'q' : q,
 
                   })
+
+def post_detail(requset, id):
+    '''
+    try:
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404
+    '''
+
+    post = get_object_or_404(Post, id=id)
+
+    return render(requset, 'blog/post_detail.html',{
+        'post': post,
+    })
