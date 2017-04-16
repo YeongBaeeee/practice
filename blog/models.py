533a191b673ab7b3bd3ffffadc9189a75485c5e2
YoungBaeeee <jjolba@naver.com>
2017-04-16 18:11:00 +0000

    blog.models.Post.tag_set 필드를 옵션필드로 지정

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
     lnglat = models.CharField(max_length=50, help_text='위도/경도 포맷으로 입력하세요.',
                               blank=True, validators=[lnglat_validator])
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    tag_set = models.ManyToManyField('Tag')
+    tag_set = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
