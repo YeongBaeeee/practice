7aff411a6fe0c515643242dcc5832036dad06657
YoungBaeeee <jjolba@naver.com>
2017-04-17 13:47:49 +0000

    accounts.models.Porfile 모델생성/admin둥록

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Porfile
+
+
+@admin.register(Porfile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+# accounts/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Porfile(models.Model):
+    user = models.OneToOneField(User) # Bad case
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
