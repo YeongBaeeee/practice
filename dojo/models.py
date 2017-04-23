d95699cfd0b095b9c601588d072e90b225eceb55
YoungBaeeee <jjolba@naver.com>
2017-04-23 16:09:58 +0000

    dojo.models.Post 모델을 추가하고, migratin

--- dojo/models.py
+++ dojo/models.py
@@ -1,1 +1,8 @@
+#dojo/models.py
 from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
