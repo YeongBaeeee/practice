e5e89d8e331042622cf1d0118cc318cc07993cd4
YoungBaeeee <jjolba@naver.com>
2017-05-02 10:43:32 +0000

    dojo.views.post_edit 뷰 구현

--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,5 @@
 # dojo/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.conf import settings
 from .forms import PostForm
@@ -46,6 +46,27 @@ def post_new(request):
                   {
                       'form' : form,
                   })
+
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            print(form.cleaned_data)
+            post = form.save(commit=False)
+            post.ip = request.META['REMOTE_ADDR']
+            post.save()
+            return redirect('/dojo/') # 혹은 namespace:name 써도됨
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'dojo/post_form.html',
+                  {
+
+                      'form' : form,
+                  })
+
 
 def mysum(request, numbers):
     return HttpResponse(sum(map(lambda s: int(s or 0), numbers.split('/'))))
