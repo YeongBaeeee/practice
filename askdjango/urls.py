c9dff80749ac296dd7fe04d0880abfc7b2798a87
YoungBaeeee <jjolba@naver.com>
2017-05-07 22:30:37 +0000

    media 파일 서빙 지원

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 from django.shortcuts import redirect
@@ -33,6 +34,8 @@ urlpatterns = [
     url(r'^dojo/', include('dojo.urls', namespace='dojo')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
