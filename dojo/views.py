81e233251fc396761e0c3b96afdf2e91a814e228
YoungBaeeee <jjolba@naver.com>
2017-05-09 14:04:54 +0000

    CBV 샘플 - STEP 3. CBV 컨셉만 구현

--- dojo/views.py
+++ dojo/views.py
@@ -129,16 +129,31 @@ def post_edit(request, id):
                   })
 
 
-def generate_view_fn(model):
-    def view_fn(request, id):
-        instance = get_object_or_404(model, id=id)
-        instance_name = model._meta.model_name
-        template_name = '{}/{}_detail.html'.format(model._meta.app_label, instance_name)
-        return render(request, template_name, {
-            instance_name: instance,
+class DetailView(object):
+    '이전 FBV를 CBV버전으로 컨셉만 간단히 구현. 같은 동작을 수행'
+    def __init__(self, model):
+        self.model = model
+
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(self.model, id=kwargs['id'])
+
+    def get_template_name(self):
+        return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
+
+    def dispatch(self, request, *args, **kwargs):
+        return render(request, self.get_template_name(), {
+            self.model._meta.model_name: self.get_object(*args, **kwargs),
         })
-    return view_fn
-post_detail = generate_view_fn(Post)
+
+    @classmethod
+    def as_view(cls, model):
+        def view(request, *args, **kwargs):
+            self = cls(model)
+            return self.dispatch(request, *args, **kwargs)
+        return view
+
+post_detail = DetailView.as_view(Post)
+
 
 def mysum(request, numbers):
     return HttpResponse(sum(map(lambda s: int(s or 0), numbers.split('/'))))
