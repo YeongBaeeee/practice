116920bcee821fa160b79e65b708c33496acd8b8
YoungBaeeee <jjolba@naver.com>
2017-05-09 16:17:40 +0000

    CBV : DeleteView 구현

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,14 @@ from . import views
 from . import views_cbv
 
 urlpatterns =[
-    url(r'^$', views_cbv.post_list, name='post_list'),
-    url(r'^(?P<id>\d+)/$', views_cbv.post_detail, name='post_detail'),
-
     url(r'^new/$', views.post_new, name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
 
+    url(r'^$', views_cbv.post_list, name='post_list'),
+    url(r'^(?P<id>\d+)/$', views_cbv.post_detail, name='post_detail'),
     url(r'^cbv/new/$', views_cbv.post_new),
     url(r'^cbv/(?P<pk>\d+)/edit/$', views_cbv.post_edit),
+    url(r'^cbv/(?P<pk>\d+)/delete/$', views_cbv.post_delete),
 ]
+
+
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -3,15 +3,17 @@
 from django.views.generic import CreateView
 from .models import Post
 from django import forms
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
 # 원래는 별로도 blog/forms.py에 구현해야하지만, 안헷갈리게 하기위해.. 나중에 수정할듯?!
 
-post_list = ListView.as_view(model=Post, paginate_by=3)
+post_list = ListView.as_view(model=Post, paginate_by=5)
 
 post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
-post_new = CreateView.as_view(model=Post)
+post_new = CreateView.as_view(model=Post, fields= '__all__')
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
+post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
